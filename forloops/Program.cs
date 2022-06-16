3159dcf8c8f1246f5e2520c3c44427e0ccfca731
Anurag1020 <anurag.mendhe14@gmail.com>
2022-06-16 21:21:17 +0530

    uc2_loops

--- forloops/Program.cs
+++ forloops/Program.cs
@@ -1,10 +1,11 @@
 ﻿
-
+const int DAILY_WAGE = 20, DAILY_HR = 8;
 Random random = new Random();
 int isemppr = random.Next(0, 2);
 if ( isemppr == 1)
 {
     Console.WriteLine(" present");
+    Console.WriteLine(DAILY_WAGE * DAILY_HR);
 }
 else
 {
